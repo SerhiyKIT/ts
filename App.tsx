2413a761810249125daa061d99893b83f4ef7048
SerhiyKIT <106843771+SerhiyKIT@users.noreply.github.com>
2022-09-30 09:43:21 +0300

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import './App.css';
+import AntdTable from './components/AntdTable';
+
+function App() {
+  return (
+    <div className="App">
+		<AntdTable/>
+    </div>
+  );
+}
+
+export default App;
