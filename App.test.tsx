45c306c1e5e7fa17f03d1b7cc79b6b92460cac76
SerhiyKIT <106843771+SerhiyKIT@users.noreply.github.com>
2022-09-30 10:11:01 +0300

    Delete App.test.tsx

--- App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { Table } from 'antd';
-
-test('renders learn react link', () => {
-  render(<Table/>);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
